2b7b4bc5be015570ee82a540e77e7ef3ac7ec1a4
Owen Brooks <owen.h.brooks@gmail.com>
2023-03-21 13:34:57 +1100

    ECE2071: Ignore error when not inside a git repository

--- makefile_include.mk
+++ makefile_include.mk
@@ -130,7 +130,7 @@ LTC_CFLAGS += -Wno-nullability-completeness
 endif
 
 
-GIT_VERSION := $(shell { [ -e .git ] && which git 2>/dev/null 1>&2 ; } && { printf git- ; git describe --tags --always --dirty ; } || echo $(VERSION))
+GIT_VERSION := $(shell { [ -e .git ] && which git 2>/dev/null 1>&2 ; } && { printf git- ; git describe --tags --always --dirty 2>/dev/null ; } || echo $(VERSION))
 ifneq ($(GIT_VERSION),)
 LTC_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 endif
